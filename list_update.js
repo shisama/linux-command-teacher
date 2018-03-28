db37d7c984bd8487a6eea6be57029bc2f57c4e26
Masashi Hirano <cherrydog07@gmail.com>
2018-03-28 23:58:28 +0900

    Add: backup

--- list_update.js
+++ list_update.js
@@ -1,5 +1,6 @@
 const cheerioClient = require('cheerio-httpcli');
 const fs = require('fs');
+const path = require('path');
 
 const getCommandList = function(){
   return searchClearly( "http://hocolamogg.com/unix/linux_com/index.html", function($){
@@ -40,6 +41,30 @@ const getCommandList = function(){
   }
 };
 
+function backup(dir, callback) {
+  return new Promise (function(resolve, reject) {
+    fs.access(dir, fs.constants.W_OK, function (err) {
+      if (err) {
+        if (err.code === "ENOENT") {
+          fs.mkdirSync(dir);
+        } else {
+          console.log("Error! backup failed.")
+          return;
+        }
+      }
+      const timestamp = Math.floor(Date.now() / 1000);
+      fs.readFile(path.join(__dirname, "list.json"), function (err, data) {
+        fs.writeFile(path.join(dir, "list.json." + timestamp), data);
+        resolve();
+      });
+    })
+  });
+}
+
+const backupdir = path.join(__dirname, "backup");
+
 getCommandList().then(function(result) {
+  return backup(backupdir);
+}).then(function() {
   fs.writeFileSync('./list.json', JSON.stringify(result.list), {encoding: 'utf-8'})
 });
